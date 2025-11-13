963054fc9e2e989288fa83b3487cba11b41eb8a4
sadiaRimi-01 <sadiaakter8138@gmail.com>
2025-11-13 20:30:09 +0600

    deploy

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC3-HlIzGPPlwDq2NwgYdmVQ2ToFEB50gQ",
-  authDomain: "eco-track-bf6a1.firebaseapp.com",
-  projectId: "eco-track-bf6a1",
-  storageBucket: "eco-track-bf6a1.firebasestorage.app",
-  messagingSenderId: "856749701887",
-  appId: "1:856749701887:web:9aa332e3f6318818142aca"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
